c3314d28cf1d91da792f2527fec366ce68535048
Muhammad Akmal Hisyam <muhammad.akmal@student.umn.ac.id>
2024-01-03 12:51:12 +0700

    feat(constants): add options constant

---
+++ src/lib/constants/options.ts
@@ -0,0 +1,32 @@
+export const ROLE_OPTIONS = [
+  { value: 'patient', label: 'Pasien' },
+  { value: 'doctor', label: 'Dokter' },
+  { value: 'nurse', label: 'Perawat' },
+];
+
+export const GENDER_OPTIONS = [
+  { value: 'male', label: 'Laki-Laki' },
+  { value: 'female', label: 'Perempuan' },
+];
+
+export const BLOOD_TYPE_OPTIONS = [
+  { value: 'A', label: 'A' },
+  { value: 'B', label: 'B' },
+  { value: 'AB', label: 'AB' },
+  { value: 'O', label: 'O' },
+];
+
+export const BLOOD_RHESUS_OPTIONS = [
+  { value: '+', label: '+' },
+  { value: '-', label: '-' },
+];
+
+export const RELIGION_OPTIONS = [
+  { value: 'Islam', label: 'Islam' },
+  { value: 'Kristen', label: 'Kristen' },
+  { value: 'Katolik', label: 'Katolik' },
+  { value: 'Hindu', label: 'Hindu' },
+  { value: 'Buddha', label: 'Buddha' },
+  { value: 'Konghucu', label: 'Konghucu' },
+  { value: 'Lainnya', label: 'Lainnya' },
+];
